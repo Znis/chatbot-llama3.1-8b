1689510fd84a694d16f76c7a56dcf25f1ba0df6e
Jenish Twayana <57634264+Znis@users.noreply.github.com>
2024-10-15 04:00:26 +0000

    chore: upload to github

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,68 @@
 import streamlit as st
-from openai import OpenAI
+import requests
+import json
+
+# Function to call the API
+def get_chat_response(user_input):
+    api_url = st.secrets['API_ENDPOINT']
+    headers = {
+        "ngrok-skip-browser-warning": "1",
+        "Content-Type": "application/json",
+        "Authorization": st.secrets['TOKEN']
+    }
+    payload = {
+        "model": st.secrets['MODEL'],
+        "messages": [
+                    { "role": "user", "content": user_input }
+                ],
+        "stream": True
+            }       
+    
+    with requests.post(api_url, json=payload, headers=headers, stream=True) as response:
+        if response.status_code == 200:
+            for line in response.iter_lines():
+                if line:
+                    data = line.decode('utf-8')
+                    data = json.loads(data)
+                    if data["message"]["content"] and not data['done']:
+                        yield data["message"]["content"]
+                    else:
+                        return
+        else:
+            yield {"error": f"Error: {response.status_code} - {response.text}"}    
+    if response.status_code == 200:
+        return response.json().get('response', 'No response from API')
+    else:
+        return f"Error: {response.status_code} - {response.text}"
 
 # Show title and description.
 st.title("💬 Chatbot")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "This is a simple chatbot similar to ChatGPT. It is just a conversational",
+    "bot with no access to internet or any other tool. 😄😄"
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+# Display the existing chat messages via `st.chat_message`.
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+# Create a chat input field to allow the user to enter a message. This will display
+# automatically at the bottom of the page.
+if prompt := st.chat_input("Ask anything"):
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+    # Store and display the current prompt.
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
-
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
+    with st.spinner("Generating response..."):
+        stream = get_chat_response(prompt)
         with st.chat_message("assistant"):
             response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+            st.session_state.messages.append({"role": "assistant", "content": response})
+
+
